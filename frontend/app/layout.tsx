ebeef39ea448a52bd6eba75e567f188726e35476
Abdul Rahman <rahmanik1228@gmail.com>
2026-07-03 02:23:49 +0500

    fix: update Meta Pixel ID to 1037461565456705

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1027835246396015');
+            fbq('init', '1037461565456705');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -52,7 +52,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1027835246396015&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1037461565456705&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
